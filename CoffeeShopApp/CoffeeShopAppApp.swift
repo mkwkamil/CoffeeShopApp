99fe97faf425545fdf67adb2870c6256f9b84702
dirtcraze <kamil.porebski@icloud.com>
2025-01-22 19:29:53 +0100

    Initial Commit

---
+++ CoffeeShopApp/CoffeeShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopAppApp.swift
+//  CoffeeShopApp
+//
+//  Created by Kamil Porębski on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
