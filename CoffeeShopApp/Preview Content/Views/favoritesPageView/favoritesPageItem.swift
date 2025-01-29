ddf30af0ef2cb3467006d4817131009562b6c513
dirtcraze <kamil.porebski@icloud.com>
2025-01-29 17:59:51 +0100

    favoriteView updated

---
+++ CoffeeShopApp/Preview Content/Views/favoritesPageView/favoritesPageItem.swift
@@ -0,0 +1,59 @@
+//
+//  favoritesPageItem.swift
+//  CoffeeShopApp
+//
+//  Created by Kamil Porębski on 29/01/2025.
+//
+
+import SwiftUI
+
+struct favoritesPageItem: View {
+    var coffee: Coffee
+    
+    var body: some View {
+        HStack {
+            coffee.image
+                .resizable()
+                .frame(width: 80, height: 80)
+                .cornerRadius(10)
+                .padding(10)
+            VStack(alignment: .leading) {
+                Text(coffee.name)
+                    .font(.headline)
+                    .foregroundColor(.black)
+                    .lineLimit(1)
+                HStack {
+                    Text("$\(coffee.price, specifier: "%.2f")")
+                    Text("\u{00B7}")
+                    Text("Size: ")
+                    Text(coffee.size[coffee.size.count - 1])
+                }
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .environment(\.locale, Locale(identifier: "en_US"))
+                .bold()
+            }
+            Spacer()
+            VStack {
+                Button {
+                    // Logic for adding coffee
+                } label: {
+                    Image(systemName: "plus")
+                        .foregroundColor(.white)
+                        .frame(width: 40, height: 40)
+                        .background(Color(red: 198 / 255, green: 124 / 255, blue: 78 / 255))
+                        .clipShape(Rectangle())
+                        .cornerRadius(12)
+                }
+            }
+            .padding()
+        }
+        .background(Color.white)
+        .cornerRadius(15)
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    favoritesPageItem(coffee: ModelData().coffees[0])
+}
