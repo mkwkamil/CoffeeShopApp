6e825be5e5debaf0021d04eae57f8c9c1a892b09
dirtcraze <kamil.porebski@icloud.com>
2025-01-31 16:14:03 +0100

    before installing fb auth

---
+++ CoffeeShopApp/Preview Content/Views/authView/authViewElements/orLoginButton.swift
@@ -0,0 +1,18 @@
+//
+//  orLoginButton.swift
+//  CoffeeShopApp
+//
+//  Created by Kamil Porębski on 31/01/2025.
+//
+
+import SwiftUI
+
+struct orLoginButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    orLoginButton()
+}
